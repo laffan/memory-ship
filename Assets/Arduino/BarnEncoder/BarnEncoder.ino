c71c93caf5a22ce334712402b64a4142135edd8d
Nate <nate@Nate.local>
2019-05-20 05:40:29 -0700

    v1. comoplete. removed aura & standard assets

--- Assets/Arduino/BarnEncoder/BarnEncoder.ino
+++ Assets/Arduino/BarnEncoder/BarnEncoder.ino
@@ -1,29 +1,132 @@
-/* Encoder Library - Basic Example
- * http://www.pjrc.com/teensy/td_libs_Encoder.html
- *
- * This example code is in the public domain.
+#include <Encoder.h>
+#include <FastLED.h>
+
+/* 
+ * Encoder Pins
+ * ----------- 
+ * CLK --> D3
+ * DT  --> D2
+ * SW  --> D4
+ * +   --> 5v
+ * GND --> GND
+ * 
  */
 
-#include <Encoder.h>
-
 Encoder myEnc(2, 3);
-const int swPin= 4 ;
+const int swPin = 4 ;
+long oldPosition  = -999;
+
+/* LEDs */
+#define LED_PIN     7
+#define NUM_LEDS    150
+CRGB leds[NUM_LEDS];
+
+/* RGB Loop */
+int cycle = 6;
+int brightness = 0;
+int maxBrightness = 130;
+int r;
+int g;
+int b;
+
 
 void setup() {
-  Serial.begin(9600);
+  
+  r = maxBrightness;
+  g = maxBrightness;
+  b = 0;
+  
   pinMode(swPin, INPUT);
+  Serial.begin(9600);
+  FastLED.addLeds<WS2812, LED_PIN, GRB>(leds, NUM_LEDS);
+
 
 }
 
-long oldPosition  = -999;
-
 void loop() {
+  // Read from rotary encoder.
   long newPosition = myEnc.read();
 
   if (newPosition != oldPosition) {
     oldPosition = newPosition;
     Serial.println(newPosition);
+    updateColor( );
   }
 
+  /* Will eventually move this inside the above if 
+   * statement so the color shift is controlled by
+   * a rotary encoder.  Down here for debugging. 
+   */
+//  delay(50);
+
+}
+
+void updateColor(  ) {
+
+//  Serial.println(" "); // For readability. 
+
+  if ( brightness == maxBrightness ) {
+    brightness = 1;
+    
+    if ( cycle < 6 ) {
+      cycle++;
+    } else {
+      cycle = 1;
+    }
+    
+  } else {
+    brightness++;
+  }
+
+  if ( cycle == 1) {
+    b++;
+  }
+  else if ( cycle == 2) { /* rb --> b */
+    r--;
+  }
+  else if ( cycle == 3) { /* b --> bg */
+    g++;
+  }
+  else if ( cycle == 4) { /* bg --> g */
+    b--;
+  }
+  else if ( cycle == 5) { /* g --> rg */
+    r++;
+  }
+  else if ( cycle == 6) { /* g --> rg */
+    g--;
+  }
+  else {
+    Serial.print("==================== ");
+  }
+
+//  Serial.print("brightness:");
+//  Serial.print(brightness);
+//  Serial.print("   cycle:");
+//  Serial.println(cycle);
+//  Serial.print("r:");
+//  Serial.print(r);
+//  Serial.print(" g:");
+//  Serial.print(g); // goes completely bonkers
+//  Serial.print(" b:");
+//  Serial.print(b);
+//  Serial.println(" ");
+//  Serial.print("r:");
+//  Serial.print(r, HEX);
+//  Serial.print(" g:");
+//  Serial.print(g, HEX); // goes completely bonkers
+//  Serial.print(" b:");
+//  Serial.print(b, HEX);
+//  Serial.println(" ");
+
+  uint8_t Ur = r;
+  uint8_t Ug = g;
+  uint8_t Ub = b;
+
+  for (int i = 0; i < NUM_LEDS; i++) {
+    leds[i].setRGB( Ur, Ug, Ub );
+  }
+
+  FastLED.show();
 
 }
--- Assets/Scripts/MediaManager.cs
+++ Assets/Scripts/MediaManager.cs
@@ -11,178 +11,174 @@ using System.Linq;
 public class MediaManager : MonoBehaviour
 {   
 
-    List<MediaFile> mediaFiles = new List<MediaFile>();
-    List<string> videoList = new List<string>();
+  List<MediaFile> mediaFiles = new List<MediaFile>();
+  List<string> videoList = new List<string>();
+  
+  public bool isRandomized = true;
 
-    public Transform imageJPGPrefab;
-    public Transform videoPrefab;
+  public Transform imageJPGPrefab;
+  public Transform videoPrefab;
 
-    public Transform mediaContainer;
-    private Vector3 currentPosition;
-    
-    public int mediaOffset;
-    public Vector3 mediaSpacing = new Vector3( 20, 0 , 0);
+  public Transform mediaContainer;
+  private Vector3 currentPosition;
+  
+  public int mediaOffset;
+  public Vector3 mediaSpacing = new Vector3( 20, 0 , 0);
 
 
-    void Start()
+
+
+  void Start()
+  {
+    currentPosition = new Vector3( 1, 1 ,1 );
+    mediaSpacing = new Vector3( mediaOffset, 0 , 0);
+    LoadData();
+
+    foreach(MediaFile media in mediaFiles)
     {
-        currentPosition = new Vector3( 1, 1 ,1 );
-        mediaSpacing = new Vector3( mediaOffset, 0 , 0);
-        LoadData();
-
-        foreach(MediaFile media in mediaFiles)
-        {
-            StartCoroutine( "SortFileTypes", media );
-        }
+      StartCoroutine( "SortFileTypes", media );
     }
+  }
 
-    private void LoadData()
+  private void LoadData()
+  {
+    string filePath = Path.Combine(Application.streamingAssetsPath, "mediaList.json");
+
+    if(File.Exists(filePath))
     {
-        string filePath = Path.Combine(Application.streamingAssetsPath, "mediaList.json");
+      string dataAsJson = File.ReadAllText(filePath); 
+      var JSONdata = JSON.Parse(dataAsJson);
 
-        if(File.Exists(filePath))
-        {
-            string dataAsJson = File.ReadAllText(filePath); 
-            var JSONdata = JSON.Parse(dataAsJson);
-
-            for (int i = 0; i < JSONdata["mediaList"].Count; i++)
-            {
-                string path = Path.Combine(Application.streamingAssetsPath, JSONdata["mediaList"][i]);
-
-                // Create new file from path
-                FileInfo newFile = new FileInfo(path);
-                
-                // Create new media file from file.
-                MediaFile newMediaFile = new MediaFile( newFile );
-
-                // Add filename to string list so you can check against it
-                // in PlayVideo() 
-                videoList.Add( newMediaFile.file.Name );
-
-                // Add to mediaFile List 
-                mediaFiles.Add( newMediaFile );
-            }
-
-        }
-        else
-        {
-            Debug.LogError("Cannot load game data!");
-        }
-
-        Debug.Log("Data Loaded");
-
+      for (int i = 0; i < JSONdata["mediaList"].Count; i++)
+      {
+        string path = Path.Combine(Application.streamingAssetsPath, JSONdata["mediaList"][i]);
+        // Create new file from path
+        FileInfo newFile = new FileInfo(path);
+        // Create new media file from file.
+        MediaFile newMediaFile = new MediaFile( newFile );
+        // Add filename to string list so you can check against it
+        // in PlayVideo() 
+        videoList.Add( newMediaFile.file.Name );
+        // Add to mediaFile List 
+        mediaFiles.Add( newMediaFile );
+      }
     }
-    
+    else
+    {
+      Debug.LogError("Cannot load game data!");
+    }
+    Debug.Log("Data Loaded");
+  }
  
-    IEnumerator SortFileTypes( MediaFile media )
+  IEnumerator SortFileTypes( MediaFile media )
+  {
+    // Look inside object
+    var output = JsonUtility.ToJson(media, true);
+    Debug.Log(output);
+      
+    if ( media.fileType == ".meta" || media.fileType == ".DS_Store" )
     {
-        // Look inside object
-        var output = JsonUtility.ToJson(media, true);
-        Debug.Log(output);
-            
-        if ( media.fileType == ".meta" || media.fileType == ".DS_Store" )
-        {
-            yield break;
-        }
-        else {
-            switch (media.fileType)
-            {
-                case ".jpg":
-                    StartCoroutine( "LoadImageJPG", media );
-                    break;
-                case ".mp4":
-                    StartCoroutine("LoadVideo", media);
-                    break;
-                default:
-                    print("Yo! Unknown extension:" + media.fileType );
-                    break;
-            }
-        }
-
+      yield break;
     }
+    else {
+      switch (media.fileType)
+      {
+        case ".jpg":
+          StartCoroutine( "LoadImageJPG", media );
+          break;
+        case ".mp4":
+          StartCoroutine("LoadVideo", media);
+          break;
+        default:
+          print("Yo! Unknown extension:" + media.fileType );
+          break;
+      }
+    }
+
+  }
+  
+  void LoadVideo(MediaFile media)
+  {
+    // Load to WWW
+    string wwwFilePath = "file://" + media.file.FullName.ToString();
     
-    void LoadVideo(MediaFile media)
+    // Instantiate prefab container
+    Transform video = (Transform) Instantiate(videoPrefab, currentPosition, transform.rotation);
+    // Name the container
+    video.transform.name = media.file.Name;
+    // Put inside mediaContainer game object.
+    video.transform.parent = mediaContainer.transform;
+
+    currentPosition = currentPosition + mediaSpacing;
+
+    // Locate correct GameObjects w/in the prefab
+    Transform label = video.Find("Label");
+    Transform surface = video.Find("Surface");
+
+    // DEBUG : Update label
+    // label.GetComponent<TextMesh>().text = media.file.FullName.ToString();
+
+    // Attach Video to surface and pause it
+    VideoClip clip = Resources.Load<VideoClip>(wwwFilePath) as VideoClip;
+    surface.GetComponent<VideoPlayer>().url = wwwFilePath;
+    surface.GetComponent<VideoPlayer>().Pause();
+  }
+
+  IEnumerator LoadImageJPG( MediaFile media )
+  {
+
+    // Load to WWW
+    string wwwFilePath = "file://" + media.file.FullName.ToString();
+    WWW www = new WWW(wwwFilePath);
+    yield return www;
+
+    // Instantiate a prefab to hold image
+    Transform jpgImage = (Transform) Instantiate(imageJPGPrefab, currentPosition, transform.rotation);
+    // Put inside mediaContainer game object.
+    jpgImage.transform.parent = mediaContainer.transform;
+     
+    currentPosition = currentPosition + mediaSpacing;
+
+    // Locate the gameObjects w/in the prefab
+    Transform label = jpgImage.Find("Label");
+    Transform surface = jpgImage.Find("Surface");
+
+    // Debug : Update label
+    label.GetComponent<TextMesh>().text = media.file.FullName.ToString();
+
+    // Apply image to texture
+    surface.GetComponent<SpriteRenderer>().sprite = Sprite.Create(www.texture, new Rect(0, 0, www.texture.width, www.texture.height), new Vector2(0.5f, 0.5f));
+
+  }
+
+  public void PlayVideo( List<string> visibleVideos ){
+   
+
+    // Play these vids
+    // var visible = videoList.Intersect(visibleVideos);
+
+    // Pause these vids
+    var invisibleVids = videoList.Except(visibleVideos);
+
+    Debug.Log( "------------------------------------------");
+    Debug.Log( "Playing " + visibleVideos.Count() + " videos");
+    Debug.Log( "Pausing " + invisibleVids.Count() + " videos");
+
+    foreach (string videoName in visibleVideos)
     {
-        // Load to WWW
-        string wwwFilePath = "file://" + media.file.FullName.ToString();
-        
-        // Instantiate prefab container
-        Transform video = (Transform) Instantiate(videoPrefab, currentPosition, transform.rotation);
-        // Name the container
-        video.transform.name = media.file.Name;
-        // Put inside mediaContainer game object.
-        video.transform.parent = mediaContainer.transform;
+      Transform media = mediaContainer.transform.Find( videoName);
+      Transform surface = media.Find("Surface");
 
-        currentPosition = currentPosition + mediaSpacing;
-
-        // Locate correct GameObjects w/in the prefab
-        Transform label = video.Find("Label");
-        Transform surface = video.Find("Surface");
-
-        // DEBUG : Update label
-        // label.GetComponent<TextMesh>().text = media.file.FullName.ToString();
-
-        // Attach Video to surface and pause it
-        VideoClip clip = Resources.Load<VideoClip>(wwwFilePath) as VideoClip;
-        surface.GetComponent<VideoPlayer>().url = wwwFilePath;
+      if ( !surface.GetComponent<VideoPlayer>().isPlaying) {
+        surface.GetComponent<VideoPlayer>().Play();
+      }
+    }
+    foreach (string videoName in invisibleVids)
+    {
+      Transform media = mediaContainer.transform.Find( videoName);
+      Transform surface = media.Find("Surface");
         surface.GetComponent<VideoPlayer>().Pause();
     }
-
-    IEnumerator LoadImageJPG( MediaFile media )
-    {
-
-        // Load to WWW
-        string wwwFilePath = "file://" + media.file.FullName.ToString();
-        WWW www = new WWW(wwwFilePath);
-        yield return www;
-
-        // Instantiate a prefab to hold image
-        Transform jpgImage = (Transform) Instantiate(imageJPGPrefab, currentPosition, transform.rotation);
-        // Put inside mediaContainer game object.
-        jpgImage.transform.parent = mediaContainer.transform;
-       
-        currentPosition = currentPosition + mediaSpacing;
-
-        // Locate the gameObjects w/in the prefab
-        Transform label = jpgImage.Find("Label");
-        Transform surface = jpgImage.Find("Surface");
-
-        // Debug : Update label
-        label.GetComponent<TextMesh>().text = media.file.FullName.ToString();
-
-        // Apply image to texture
-        surface.GetComponent<SpriteRenderer>().sprite = Sprite.Create(www.texture, new Rect(0, 0, www.texture.width, www.texture.height), new Vector2(0.5f, 0.5f));
-
-    }
-
-    public void PlayVideo( List<string> visibleVideos ){
-     
-
-        // Play these vids
-        // var visible = videoList.Intersect(visibleVideos);
-
-        // Pause these vids
-        var invisibleVids = videoList.Except(visibleVideos);
-
-        Debug.Log( "------------------------------------------");
-        Debug.Log( "Playing " + visibleVideos.Count() + " videos");
-        Debug.Log( "Pausing " + invisibleVids.Count() + " videos");
-
-        foreach (string videoName in visibleVideos)
-        {
-            Transform media = mediaContainer.transform.Find( videoName);
-            Transform surface = media.Find("Surface");
-
-            if ( !surface.GetComponent<VideoPlayer>().isPlaying) {
-                surface.GetComponent<VideoPlayer>().Play();
-            }
-        }
-        foreach (string videoName in invisibleVids)
-        {
-            Transform media = mediaContainer.transform.Find( videoName);
-            Transform surface = media.Find("Surface");
-                surface.GetComponent<VideoPlayer>().Pause();
-        }
-    }
+  }
 }
 
